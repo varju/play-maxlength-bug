bff7b852135ce9db7a8ef623ebc393034a0db23f
Alex Varju <alex@varju.ca>
2013-07-18 12:27:36 -0700

    demonstrate bug

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,5 +8,11 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+  def maxlen = Action(parse.maxLength(5, parser = parse.anyContent)) { implicit request =>
+    request.body match {
+      case Left(maxSize) => BadRequest("exceeded maxSize " + maxSize)
+      case Right(body) => Ok("accepted")
+    }
+  }
 }
